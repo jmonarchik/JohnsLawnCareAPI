b9397db90201629ebcdedad75333678d8842f3bb
Ricky Wilbanks <rdwilbanks3@gmail.com>
2017-05-22 18:17:54 -0400

    added tables and corresponding events for admin portal and track order portal

--- FrontEnd/src/app/components/orders/orders.component.ts
+++ FrontEnd/src/app/components/orders/orders.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
+import { Order } from '../../models/order';
 
 @Component({
     selector: 'app-orders',
 
-    template:`
-        <span>Orders Works!</span>
-    `,
+    templateUrl:'orders.component.html',
 
     styles:[`
     
     `],
 })
 export class OrdersComponent {
-
+orders: Order[] =[
+    new Order("Wrights Pizzeria","4","pepperoni, peppers", "out for delivery","Havel","$16.99",
+    "(404)452-6512","4:32 PM"),
+]
 }
