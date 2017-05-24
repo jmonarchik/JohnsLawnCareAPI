a122db53a24307bc9f658014a899b34cce4af7a0
Ricky Wilbanks <rdwilbanks3@gmail.com>
2017-05-24 17:28:43 -0400

    rendering topping checkboxes dynamically

--- FrontEnd/src/app/components/create-order/create-order.component.ts
+++ FrontEnd/src/app/components/create-order/create-order.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Topping } from '../../models/topping';
+import { Store } from '@ngrx/store';
+import { ToppingActions } from '../../actionHandlers/topping.actions';
 
 @Component({
     selector: 'app-create-order',
-
     templateUrl: 'create-order.component.html',
-
     styleUrls: ['create-order.component.css'],
 })
 export class CreateOrderComponent {
-    toppings: Topping[] = [];
+    toppings: Array<Topping>;
+    toppingsSubscription: any;
+    isToppingsTableVisible: boolean = false;
+    selectedToppings: Array<number> = [];
+
+    constructor(private _store: Store<any>, private _toppingActions: ToppingActions) { }
+
+    public ngOnInit() {
+        this.toppingsSubscription = this._store.select('toppings').subscribe((toppings: Array<Topping>) => {
+            this.toppings = toppings;
+        });
+
+        this._toppingActions.getToppings();
+    }
+    public ngDoCheck() {
+        console.log(this.selectedToppings);
+    }
+    public ngOnDestroy() {
+        this.toppingsSubscription.unsubscribe();
+    }
+    public selectTopping(toppingId: number): void {
+        //debugger;
+        const index = this.selectedToppings.indexOf(toppingId);
+        if(index !== -1){
+            this.selectedToppings.splice(index, 1);
+        }
+        else{
+            this.selectedToppings.push(toppingId);
+        }
+        console.log(this.selectedToppings);
+    }
 }
